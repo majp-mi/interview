b2f6b63c2762f8f6761b66b72049328fdea5bb7e
马金鹏 <496096158@qq.com>
2020-04-24 09:07:23 +0800

    ArrayList线程不安全解决方案1

---
+++ day01/src/com/test/volatileDemo/ListDemo.java
@@ -0,0 +1,23 @@
+package com.test.volatileDemo;
+
+import java.util.*;
+
+/**
+ * ArrayList线程不安全
+ * 解决方法：1.使用Vector;
+ *          2.使用Collections.synchronizedList(new ArrayList<>())
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+
+       // java.util.ConcurrentModificationException
+//        List<String> list = new ArrayList<String>();
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        for (int i = 0; i < 30; i++) {
+            new Thread(()->{
+                list.add(UUID.randomUUID().toString().substring(0,8));
+                System.out.println(list);
+            },String.valueOf(i)).start();
+        }
+    }
+}
