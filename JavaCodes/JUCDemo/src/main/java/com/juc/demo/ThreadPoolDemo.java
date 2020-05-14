340e28e3d30bcfc5478aa68c7b4ee04dce0722d0
马金鹏 <496096158@qq.com>
2020-05-14 18:52:51 +0800

    手写线程池ThreadPoolExecutor

--- JavaCodes/JUCDemo/src/main/java/com/juc/demo/ThreadPoolDemo.java
+++ JavaCodes/JUCDemo/src/main/java/com/juc/demo/ThreadPoolDemo.java
@@ -1,7 +1,6 @@
 package com.juc.demo;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @ClassName ThreadPoolDemo
@@ -19,18 +18,29 @@ public class ThreadPoolDemo {
         //以下三种创建线程池的方式在实际工作中一个都不用
 //        ExecutorService executor = Executors.newFixedThreadPool(3); //创建固定线程数,会导致队列积压大量请求，导致OOM
 //        ExecutorService executor = Executors.newSingleThreadExecutor(); //创建单个线程数,会导致队列积压大量请求，导致OOM
-        ExecutorService executor = Executors.newCachedThreadPool(); //创建可变线程数，会创建出大量的线程，导致OOM
+//        ExecutorService executor = Executors.newCachedThreadPool(); //创建可变线程数，会创建出大量的线程，导致OOM
+//        ThreadPoolExecutor pool = new ThreadPoolExecutor();
+        // CallerRunsPolicy回退调用者机制
+        ExecutorService pool = new ThreadPoolExecutor(2,40,1, TimeUnit.SECONDS,new LinkedBlockingDeque<>(3),Executors.defaultThreadFactory(),new ThreadPoolExecutor.CallerRunsPolicy());
         try {
             for (int i = 1; i <= 10; i++) {
-                executor.execute(() -> {
+                pool.execute(() -> {
                     System.out.println(Thread.currentThread().getName() + "处理请求");
                 });
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            executor.shutdown();
+            pool.shutdown();
         }
+        /**
+         * 合理线程数配置：
+         * cpu密集型：cpu核数+1
+         * IO密集型：cpu核数/（1-阻塞系数）  阻塞系数一般为0.8~0.9
+         */
+        System.out.println("cpu核数：");
+        //4
+        System.out.println(Runtime.getRuntime().availableProcessors());
 
 
     }
